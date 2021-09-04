ce4d24fc671f46e593dee8f2850c3b6c8353b64e
largentba21 <largentba21@gmail.com>
2021-09-04 16:39:54 -0500

    cleaning up main.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
 import App from "./App.svelte";
 
-// $("#chat-form").after(`<div id="dnd5e-simple-dice-tray"></div>`);
-// const target = $("#dnd5e-simple-dice-tray")[0];
-// new App({ target });
-
 Hooks.on("renderChatLog", (app, html, data) => {
   html.find("#chat-form").after(`<div id="dnd5e-simple-dice-tray"></div>`);
   const target = html.find("#dnd5e-simple-dice-tray")[0];
   new App({ target });
 });
-
-// class MyApp extends Application {
-//   activateListeners(html) {
-//     html.find("#chat-form").after(`<div id="dnd5e-simple-dice-tray"></div>`);
-//     const target = html.find("#dnd5e-simple-dice-tray")[0];
-//     this.component = new App({
-//       target,
-//       props: {
-//         name: "Foundry",
-//       },
-//     });
-//   }
-// }
-
-// Hooks.on("ready", () => {
-//   const app = new MyApp();
-//   app.render();
-// });
