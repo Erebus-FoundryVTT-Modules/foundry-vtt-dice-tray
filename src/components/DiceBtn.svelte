442f9af7d944ddea76c5234a895ae556014aa535
largentba21 <largentba21@gmail.com>
2021-09-18 14:06:26 -0500

    re-refactoring to use a sprite sheet for dice in buttons

--- src/components/DiceBtn.svelte
+++ src/components/DiceBtn.svelte
@@ -1,21 +1,6 @@
 <script>
-  import D4 from "./D4.svelte";
-  import D6 from "./D6.svelte";
-  import D8 from "./D8.svelte";
-  import D10 from "./D10.svelte";
-  import D12 from "./D12.svelte";
-  import D20 from "./D20.svelte";
   import { dicePool } from "../store";
   export let die;
-
-  const dieMap = {
-    4: D4,
-    6: D6,
-    8: D8,
-    10: D10,
-    12: D12,
-    20: D20,
-  };
 
   function handleKeyup({ key }) {
     if ({ Delete: 1, Backspace: 1 }[key]) {
@@ -41,7 +26,11 @@
   {#if die == 100}
     100
   {:else}
-    <svelte:component this={dieMap[die]} />
+    <svg width="20" height="20">
+      <use
+        xlink:href="modules/dnd5e-simple-dice-tray/assets/dice-sprite.svg#d{die}"
+      />
+    </svg>
   {/if}
 </button>
 
